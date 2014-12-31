9662161335a1da68122fcfa8c8d90e0b6b785c2f
Steven Weaver <sweaver@ucsd.edu>
2014-12-30 16:14:32 -0800

    adding 'binary-select' option for times when attributes are mutually exclusive

--- phylotree.js
+++ phylotree.js
@@ -47,7 +47,9 @@ d3.layout.phylotree = function (container) {
                                     'y-spacing' : 'fit-to-size',
                                     'x-spacing' : 'fixed-width',
                                     'show-scale' : 'top',
-                                    'draw-size-bubbles': false
+                                    'draw-size-bubbles': false,
+                                    'binary-selectable': false,
+                                    'attribute-list': []
                                   },
                                   
         css_classes             = {'tree-container': 'phylotree-container',
@@ -332,7 +334,7 @@ d3.layout.phylotree = function (container) {
     phylotree.selection_label = function(attr) {
         if (!arguments.length) return selection_attribute_name;
         selection_attribute_name = attr;
-        phylotree.sync_edge_labels ();
+        phylotree.sync_edge_labels();
         return phylotree;
     };
         
@@ -441,6 +443,7 @@ d3.layout.phylotree = function (container) {
     };
     
     phylotree.get_newick = function (annotator) {
+
         function node_display (n) {
             if (!d3_phylotree_is_leafnode (n)) {
                 element_array.push ("(");
@@ -462,14 +465,15 @@ d3.layout.phylotree = function (container) {
             
         }
     
-    
         var element_array = [],
         annotator = annotator || "";  
         node_display (nodes[0]);
         return element_array.join ("");
+
     }
     
     phylotree.update_layout = function (new_json, do_hierarchy) {
+
         if (do_hierarchy) {
             nodes = d3_hierarchy.call (this, new_json);
             nodes.forEach (function (d) { d.id = null;});
@@ -477,6 +481,7 @@ d3.layout.phylotree = function (container) {
         phylotree.placenodes();
         links = phylotree.links (nodes);
         phylotree.sync_edge_labels ();
+
     }
     
     phylotree.sync_edge_labels = function () {
@@ -486,7 +491,8 @@ d3.layout.phylotree = function (container) {
             d.tag = d.target.tag || false;
         });
             
-        d3_phylotree_trigger_refresh      (phylotree);
+        d3_phylotree_trigger_refresh(phylotree);
+
         if (phylotree.count_handler()) {
             var counts = {};
             counts[selection_attribute_name] = links.reduce (function (p, c) { return p + (c[selection_attribute_name] ? 1 : 0);}, 0);
@@ -495,16 +501,16 @@ d3.layout.phylotree = function (container) {
             d3_phylotree_trigger_count_update (phylotree, 
             counts, phylotree.count_handler());
         }     
+
     };
-    
-    
 
     phylotree.modify_selection = function (callback, attr, place, skip_refresh, mode) {
             
         attr = attr || selection_attribute_name;        
         mode = mode || "toggle";
         
-        if (options["selectable"]) {
+        if (options["selectable"] && !options["binary-selectable"] ) {
+
             var do_refresh = false;
         
             if (typeof callback === 'function') {
@@ -518,7 +524,9 @@ d3.layout.phylotree = function (container) {
                         d.target[attr] = select_me;
                     }
                 });
-            } else {
+
+            }  else {
+
                 callback.forEach (function (d) {
                     var new_value; 
                     switch (mode) {
@@ -549,7 +557,6 @@ d3.layout.phylotree = function (container) {
         
             if (do_refresh) {
                 if (!skip_refresh) {
-                    //console.log ("Refresh");
                     d3_phylotree_trigger_refresh      (phylotree);
                 }
                 if (phylotree.count_handler()) {
@@ -564,6 +571,76 @@ d3.layout.phylotree = function (container) {
                     phylotree.placenodes();
                 }
             }    
+
+        } else if (options['binary-selectable']) {
+
+            if (typeof callback === 'function') {
+                links.forEach (function (d) {
+
+                    var select_me = callback (d);
+                    d[attr] = d[attr] || false;
+
+
+                    if (d[attr] != select_me) {
+                        d[attr] = select_me;
+                        do_refresh = true;
+                        d.target[attr] = select_me;
+                    }
+
+                    options['attribute-list'].forEach(function(type) {
+                      if(type != attr && d[attr] == true) {
+                        d[type] = false;
+                        d.target[type] = false;
+                      }
+
+                    });
+
+                });
+
+            }  else {
+
+              callback.forEach (function (d) {
+
+                 var new_value; 
+                 new_value = !d[attr];
+
+                 if (d[attr] != new_value) {
+                     d[attr] = new_value;
+                     do_refresh = true;
+                 }
+
+              });
+              
+              links.forEach (function(d) {
+                  d[attr] = d.target[attr];
+                  options['attribute-list'].forEach(function(type) {
+                    if(type != attr && d[attr] == true) {
+                      d[type] = false;
+                      d.target[type] = false;
+                    }
+
+                  });
+
+              });
+            }
+
+            if (do_refresh) {
+                if (!skip_refresh) {
+                    d3_phylotree_trigger_refresh      (phylotree);
+                }
+                if (phylotree.count_handler()) {
+                    var counts = {};
+                    counts[attr] = links.reduce (function (p, c) { return p + (c[attr] ? 1 : 0);}, 0);
+                    d3_phylotree_trigger_count_update (phylotree, 
+                    counts, 
+                    phylotree.count_handler());
+                }
+                
+                if (place) {
+                    phylotree.placenodes();
+                }
+            }    
+
         }
         
         return phylotree;
@@ -1171,7 +1248,9 @@ d3.layout.phylotree = function (container) {
       container = d3.select(container);
       
       container.attr("class", phylotree.reclass_edge)
-               .on ("click", function (d) { phylotree.modify_selection ([d.target], selection_attribute_name); });
+               .on ("click", function (d) { 
+                  phylotree.modify_selection ([d.target], selection_attribute_name); 
+                });
                 
       var new_branch_path = draw_branch ([edge.source, edge.target]);
                
