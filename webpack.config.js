d76490ff3daf5d5834de0bbb85275c919205700b
Stephen Shank <sshank314@gmail.com>
2018-05-04 15:53:41 -0400

    Configures webpack for production, adds bundle for Github pages.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: path.resolve(__dirname, 'phylotree.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
