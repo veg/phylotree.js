f8a8b8c16bfb99a4188ad7cde0ea1ed17a86508c
rdvelazquez <ryan.velazquez0001@temple.edu>
2018-09-12 11:19:30 -0400

    address sinlge character node names; only publish the desired files

--- src/main.js
+++ src/main.js
@@ -2220,7 +2220,7 @@ d3.layout.phylotree = function(container) {
     var width = 0;
 
     nodes.filter(d3_phylotree_node_visible).forEach(function(node) {
-      var node_width = node_label(node).length * _font_size * 0.8;
+      var node_width = 12 + node_label(node).length * _font_size * 0.8;
       if (node.angle !== null) {
         node_width *= Math.max(
           Math.abs(Math.cos(node.angle)),
