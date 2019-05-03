167ad5b85974245ca54e9a7aa9dc16c00bc13381
Steven Weaver <sweaver@temple.edu>
2019-05-03 17:11:27 -0400

    fixes #120: reroot render issue

--- src/rooting.js
+++ src/rooting.js
@@ -11,25 +11,25 @@ import * as _ from "underscore";
 * @returns {Phylotree} The current ``phylotree``.
 */
 export function reroot(node, fraction) {
+
   /** TODO add the option to root in the middle of a branch */
 
   let nodes = this.nodes.descendants();
 
   fraction = fraction !== undefined ? fraction : 0.5;
 
-
   if (node.parent) {
+
     var new_json = d3.hierarchy({
       name: "new_root",
       //__mapped_bl: undefined,
       "children": [{
-      	name : node.data.name
+          name : node.data.name
       }]
     });
     
     _.extendOwn (new_json.children[0], node);
     new_json.children[0].parent = new_json;
-    
 
     nodes.forEach(n => {
       n.__mapped_bl = this.branch_length_accessor(n);
@@ -46,20 +46,21 @@ export function reroot(node, fraction) {
       stashed_bl = _.noop();
 
     let apportioned_bl =
-      node.__mapped_bl === undefined ? undefined : node.__mapped_bl * fraction;
+      node.data.__mapped_bl === undefined ? undefined : node.data.__mapped_bl * fraction;
 
-    stashed_bl = current_node.__mapped_bl;
+    stashed_bl = current_node.data.__mapped_bl;
 
-    current_node.__mapped_bl =
-      node.__mapped_bl === undefined
+    current_node.data.__mapped_bl =
+      node.data.__mapped_bl === undefined
         ? undefined
         : node.__mapped_bl - apportioned_bl;
 
-    node.__mapped_bl = apportioned_bl;
+    node.data._mapped_bl = apportioned_bl;
 
     var remove_idx;
 
     if (current_node.parent) {
+
       new_json.children.push(current_node);
 
       while (current_node.parent) {
@@ -70,9 +71,10 @@ export function reroot(node, fraction) {
           current_node.children.splice(remove_idx, 1);
         }
 
-        let t = current_node.parent.__mapped_bl;
+        let t = current_node.parent.data.__mapped_bl;
+
         if (t !== undefined) {
-          current_node.parent.__mapped_bl = stashed_bl;
+          current_node.parent.data.__mapped_bl = stashed_bl;
           stashed_bl = t;
         }
         remove_me = current_node;
@@ -83,22 +85,21 @@ export function reroot(node, fraction) {
     } else {
       remove_idx = current_node.children.indexOf(remove_me);
       current_node.children.splice(remove_idx, 1);
-      stashed_bl = current_node.__mapped_bl;
+      stashed_bl = current_node.data.__mapped_bl;
       remove_me = new_json;
     }
 
     // current_node is now old root, and remove_me is the root child we came up
     // the tree through
-
     if (current_node.children.length == 1) {
       if (stashed_bl) {
-        current_node.children[0].__mapped_bl += stashed_bl;
+        current_node.children[0].data.__mapped_bl += stashed_bl;
       }
       remove_me.children = remove_me.children.concat(current_node.children);
     } else {
       let new_node = new d3.hierarchy({ name: "__reroot_top_clade", __mapped_bl: stashed_bl });
       _.extendOwn (new_json.children[0], node);
-      new_node.__mapped_bl = stashed_bl;
+      new_node.data.__mapped_bl = stashed_bl;
       new_node.children = current_node.children.map(function(n) {
         n.parent = new_node;
         return n;
@@ -106,16 +107,27 @@ export function reroot(node, fraction) {
       new_node.parent = remove_me;
       remove_me.children.push(new_node);
      }
+
   }
   
   // need to traverse the nodes and update parents
-  
-
   this.update(new_json);
+
   this.traverse_and_compute(n => {
     _.each (n.children, (c) => {c.parent = n;})
-  }, "pre-order")
+  }, "pre-order");
+
+	if(!_.isUndefined(this.display)) {
+		// get container
+		let container = this.display.container;
+		let options = this.display.options;
+		// get options
+		delete this.display;
+  	this.render(container, options);
+	}
+
   return this;
+
 }
 
 export function rootpath(attr_name, store_name) {
