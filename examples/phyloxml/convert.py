e05dbb9daaad6d6bbcb1b4c019e061c3d629b4c4
Steven Weaver <sweaver@temple.edu>
2021-05-10 08:43:35 -0700

    removing outdated examples

--- examples/phyloxml/convert.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-from Bio import Phylo
-
-xml_filename = argv[1] + '.xml'
-newick_filename = argv[1] + '.new'
-
-tree = Phylo.read(xml_filename, 'phyloxml')
-Phylo.write(tree, newick_filename, 'newick')
