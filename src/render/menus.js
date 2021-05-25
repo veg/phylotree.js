fc0b1960f5e64250c364b82a6776953d75f38ff8
Steven Weaver <sweaver@temple.edu>
2021-05-24 17:07:34 -0700

    closes #363 -- better window positioning

--- src/render/menus.js
+++ src/render/menus.js
@@ -210,8 +210,8 @@ export function nodeDropdownMenu(node, container, phylotree, options) {
 
     menu_object
       .style("position", "absolute")
-      .style("left", "" + coordinates[0] + "px")
-      .style("top", "" + coordinates[1] + "px")
+      .style("left", "" + (coordinates[0] + tree_container.position().left) + "px")
+      .style("top", "" + (coordinates[1] + tree_container.position().top) + "px")
       .style("display", "block");
   } else {
     menu_object.style("display", "none");
