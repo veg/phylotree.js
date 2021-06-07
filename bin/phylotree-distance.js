e96f9e4c6a8d823be1b701650ba1373126764211
Steven Weaver <sweaver@temple.edu>
2021-06-07 16:46:22 -0700

    add include source length option to distance calculation

--- bin/phylotree-distance.js
+++ bin/phylotree-distance.js
@@ -9,6 +9,7 @@ commander
   .arguments("<newick>", "Input newick file")
   .requiredOption("-s --source <node>", "Source Node")
   .requiredOption("-t --target <node>", "Target Node")
+  .option("-i --include-source-length", "Include source node's length as part of distance")
   .usage("./test/data/CD2-relax.new  -s Cow -t Cat")
   .on("--help", function() {
     console.log("\nComputes the distance from the source tip to target tip. Does *not* include the length of the source tip.");
@@ -31,7 +32,12 @@ fs.readFile(commander.args[0], (err, newick_data) => {
   const targetNode = tree.getNodeByName(commander.target);
   const targetIndex = targetNode['cot_leaf_index'];
 
-  const distance = sourceNode['cot_path_to_leaves_above'][targetIndex];
+  let distance = sourceNode['cot_path_to_leaves_above'][targetIndex];
+
+  // if include source tip length, add it
+  if(commander.includeSourceLength) {
+    distance+=parseFloat(sourceNode.data.attribute);
+  }
   console.log(distance);
 
 });
