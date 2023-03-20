5522cf20ed7ae6bcf98eb0206f8cecd47cc10d75
Steven Weaver <sweaver@temple.edu>
2023-03-20 10:54:46 -0600

    version bump

--- bin/phylotree.js
+++ bin/phylotree.js
@@ -5,7 +5,7 @@ const phylotree = require('../dist/phylotree.js');
 const { program } = require('commander');
 
 program
-  .version('1.2.2')
+  .version('1.2.3')
   .command('tips', 'get tips from newick tree')
   .command('validate', 'validate newick string')
   .command('reroot', 'reroot newick tree based on node')
