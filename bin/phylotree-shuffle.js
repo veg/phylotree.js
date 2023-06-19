c40c88877d55ed76e263cdc2526a8d44bd81c2d0
Steven Weaver <sweaver@temple.edu>
2023-06-19 15:25:03 -0600

    v1.4.0

---
+++ bin/phylotree-shuffle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const fs = require("fs"),
+  phylotree = require("../dist/phylotree.js"),
+  commander = require("commander"),
+  _ = require("underscore");
+
+commander
+  .arguments("<newick>", "Input newick file")
+  .on("--help", function() {
+    console.log("");
+    console.log("Examples:");
+    console.log(
+      'phylotree tips test/data/MERS.txt'
+    );
+  })
+  .parse(process.argv);
+
+fs.readFile(commander.args[0], (err, newickData) => {
+
+  const tree = new phylotree.phylotree(newickData.toString());
+  let tips = tree.getTips();
+
+  // Get all the names and randomly assign them
+  let names = _.map(tips, d => d.data.name);
+
+  // Shuffle names
+  let shuffledNames = _.shuffle(names);
+
+  _.each(tips, (d,i) => d.data.name = shuffledNames[i])
+
+  console.log(tree.getNewick());
+
+});
